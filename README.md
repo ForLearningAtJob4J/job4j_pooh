7bce39a6f9af44b4a239df6314edcf88cfab1e4b
beeebeep <YouCanWriteMe@ya.ru>
2021-03-09 23:01:58 +0700

    1. Техническое задание - проект "Pooh JMS" [#461452]

---
+++ README.md
@@ -0,0 +1,3 @@
+# job4j_pooh
+[![Build Status](https://travis-ci.org/ForLearningAtJob4J/job4j_pooh.svg?branch=master)](https://travis-ci.org/ForLearningAtJob4J/job4j_pooh)
+[![codecov](https://codecov.io/gh/ForLearningAtJob4J/job4j_pooh/branch/master/graph/badge.svg)](https://codecov.io/gh/ForLearningAtJob4J/job4j_pooh)
---
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -0,0 +1,21 @@
+package ru.job4j.pooh;
+
+public class Req {
+    public final String text;
+
+    public Req(String text) {
+        this.text = text;
+    }
+
+    public String valueOf(String key) {
+        return null;
+    }
+
+    public String mode() {
+        return null;
+    }
+
+    public String method() {
+        return null;
+    }
+}
